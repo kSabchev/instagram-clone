549ee19cd818474f5ecff6a6eed143c0f846fa94
kSabchev <kaloyansabchev@gmail.com>
2020-08-10 23:43:17 +0300

    testing header

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+       Lets build instagram
+      </h1>
+      <div className="app__header">
+        <img 
+          className="app__headerImage"
+          src="https://i.pinimg.com/564x/f9/f0/7a/f9f07a203874e68dd521ff8f8b12d0b7.jpg"
+        />  
+      </div>
+      {/* Header */}
+      {/* Posts */}
+      {/* Posts */}
+      
     </div>
   );
 }
